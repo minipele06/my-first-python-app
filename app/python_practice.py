a52738122192dc6c53a39fc1929f145ec6a4175f
minipele06 <aja398@stern.nyu.edu>
2020-05-26 20:50:55 -0400

    Create python_practice.py

---
+++ app/python_practice.py
@@ -0,0 +1,38 @@
+
+
+
+
+x = 1
+print(x)
+
+
+# breakpoint() # for temporary debugging purposes
+
+x = 10
+print(x)
+
+y = 5
+print(y)
+
+y+=100
+print(y)
+
+print(1,2,3)
+
+z = 12
+
+if z == 37:
+    print("YES")
+else:
+    print("NO")
+
+print(type(x))
+
+arr = ["a","b","c","d"]
+
+print(arr[0])
+
+print(arr.count)
+
+for letter in ["a", "b", "c", "d"]:
+    print(letter)
